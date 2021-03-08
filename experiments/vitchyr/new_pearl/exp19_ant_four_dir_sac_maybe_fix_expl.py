3d76b12fe5b4a04124a7f36850b7373c69e2056b
Vitchyr Pong <vitchyr@gmail.com>
2021-03-08 11:22:38 -0800

    redo cql (exp15) and fix expl for refactoed ant direction code (exp19)...I think

---
+++ experiments/vitchyr/new_pearl/exp19_ant_four_dir_sac_maybe_fix_expl.py
@@ -0,0 +1,81 @@
+import click
+from pathlib import Path
+
+from rlkit.launchers.launcher_util import run_experiment, load_pyhocon_configs
+import rlkit.pythonplusplus as ppp
+from rlkit.torch.pearl.new_sac_launcher import pearl_sac_experiment
+import rlkit.misc.hyperparameter as hyp
+
+
+@click.command()
+@click.option('--debug', is_flag=True, default=False)
+@click.option('--dry', is_flag=True, default=False)
+@click.option('--suffix', default=None)
+def main(debug, dry, suffix):
+    mode = 'sss'
+    n_seeds = 3
+    gpu = True
+
+    base_dir = Path(__file__).parent
+    configs = [
+        base_dir / 'configs/default_sac.conf',
+        base_dir / 'configs/ant_four_dir_nupo1.conf',
+    ]
+
+    path_parts = __file__.split('/')
+    suffix = '' if suffix is None else '--{}'.format(suffix)
+    exp_name = '{}--{}{}'.format(
+        path_parts[-2].replace('_', '-'),
+        path_parts[-1].split('.')[0].replace('_', '-'),
+        suffix,
+    )
+    if debug:
+        configs.append(base_dir / 'configs/debug.conf')
+    if debug or dry:
+        exp_name = 'dev--' + exp_name
+        mode = 'local'
+        n_seeds = 1
+
+    config = load_pyhocon_configs(configs)
+    variant = ppp.recursive_to_dict(config)
+    # variant.pop('name_to_expl_path_collector_kwargs')
+
+    search_space = {
+        'save_video': [True, False],
+        'logger_config.snapshot_mode': ['gap_and_last'],
+        'logger_config.snapshot_gap': [25],
+
+        'save_video_kwargs.video_img_size': [64],
+        # 'name_to_expl_path_collector_kwargs': [
+        #     dict(
+        #         init_from_buffer=dict(
+        #             sample_initial_context=True,
+        #             accum_context_across_rollouts=False,
+        #             resample_latent_period=0,
+        #             update_posterior_period=0,
+        #             use_predicted_reward=False,
+        #         )
+        #     )
+        # ]
+    }
+    sweeper = hyp.DeterministicHyperparameterSweeper(
+        search_space, default_parameters=variant,
+    )
+    for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+        for _ in range(n_seeds):
+            variant['exp_id'] = exp_id
+            run_experiment(
+                pearl_sac_experiment,
+                unpack_variant=True,
+                exp_name=exp_name,
+                mode=mode,
+                variant=variant,
+                time_in_mins=3 * 24 * 60 - 1,
+                use_gpu=gpu,
+            )
+    print(exp_name)
+
+
+if __name__ == "__main__":
+    main()
+
