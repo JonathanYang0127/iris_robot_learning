642824551740f4ff15798b8dc6ce23a4aefb4a54
Albert Yu <albertyu@berkeley.edu>
2021-06-09 10:33:33 -0700

    Replace timestep variable with flag in eval.

--- experiments/avi/eval_wx250.py
+++ experiments/avi/eval_wx250.py
@@ -13,11 +13,11 @@ import rlkit.torch.pytorch_util as ptu
 
 if __name__ == '__main__':
     num_trajs = 100
-    num_timesteps = 10
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--checkpoint_path", type=str, required=True)
-    parser.add_argument("-v", "--video_save_dir", type=str, default="")
+    parser.add_argument("-c", "--checkpoint-path", type=str, required=True)
+    parser.add_argument("-v", "--video-save-dir", type=str, default="")
+    parser.add_argument("--num-timesteps", type=int, default=10)
     args = parser.parse_args()
 
     if not os.path.exists(args.video_save_dir):
@@ -49,7 +49,7 @@ if __name__ == '__main__':
             input("Press Enter to continue...")
         print("Eval Traj {}".format(i))
 
-        for j in range(num_timesteps):
+        for j in range(args.num_timesteps):
             action, info = eval_policy.get_action(obs_flat)
             print(action[-1])
             # if j > 7:
