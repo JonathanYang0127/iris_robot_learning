e475ba86d7c64c584223c0fb507b2eb59ec02176
Albert Yu <albertyu@berkeley.edu>
2021-06-09 19:18:11 -0700

    Remove single Q function from awac launch scripts.

--- experiments/avi/awac_image_wx250.py
+++ experiments/avi/awac_image_wx250.py
@@ -73,12 +73,8 @@ def experiment(variant):
 
     qf1 = ConcatCNN(**cnn_params)
     qf2 = ConcatCNN(**cnn_params)
-    if variant['use_single_q']:
-        target_qf1 = None
-        target_qf2 = None
-    else:
-        target_qf1 = ConcatCNN(**cnn_params)
-        target_qf2 = ConcatCNN(**cnn_params)
+    target_qf1 = ConcatCNN(**cnn_params)
+    target_qf2 = ConcatCNN(**cnn_params)
 
     replay_buffer = ObsDictReplayBuffer(
         int(1E6),
@@ -161,7 +157,6 @@ if __name__ == '__main__':
     parser.add_argument('--use-robot-state', action='store_true', default=False)
     parser.add_argument('--use-negative-rewards', action='store_true',
                         default=False)
-    parser.add_argument('--use-single-q', action='store_true', default=False)
     parser.add_argument("--gpu", default='0', type=str)
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--use-bc", action="store_true", default=False)
@@ -240,7 +235,6 @@ if __name__ == '__main__':
         image_augmentation=True,
         image_augmentation_padding=4,
     )
-    variant['use_single_q'] = args.use_single_q
     variant['seed'] = args.seed
     variant['use_bc'] = args.use_bc
     if args.num_trajs_limit > 0:
