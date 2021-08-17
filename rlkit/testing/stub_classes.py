6ed0e2920b481541fbfa02cf51672ea9db8c18d5
Ashvin Nair <nair.ashvin@gmail.com>
2021-08-17 12:27:27 -0700

    Refactor testing code

--- rlkit/testing/stub_classes.py
+++ rlkit/testing/stub_classes.py
@@ -1,21 +1,68 @@
 import numpy as np
-from gym.spaces import Box
+from gym.spaces import Box, Dict
 
 from rlkit.exploration_strategies.base import RawExplorationStrategy
 
-
 class StubEnv(object):
-    def __init__(self):
-        low = np.array([0.])
-        high = np.array([1.])
-        self._action_space = Box(low, high)
-        self._observation_space = Box(low, high)
+    def __init__(self, obs_dim=1, action_dim=1, **kwargs):
+        self.obs_dim = obs_dim
+        obs_low = np.ones(obs_dim) * -1
+        obs_high = np.ones(obs_dim)
+        self._observation_space = Box(obs_low, obs_high)
+
+        self.action_dim = action_dim
+        action_low = np.ones(action_dim) * -1
+        action_high = np.ones(action_dim)
+        self._action_space = Box(action_low, action_high)
+
+        print("unused kwargs", kwargs)
 
     def reset(self):
         pass
 
     def step(self, action):
-        return 0, 0, 0, {}
+        return np.zeros(self.obs_dim), 0, 0, {}
+
+    @property
+    def action_space(self):
+        return self._action_space
+
+    @property
+    def horizon(self):
+        return 99999
+
+    @property
+    def observation_space(self):
+        return self._observation_space
+
+class StubMultiEnv(object):
+    def __init__(self, obs_dims=None, action_dim=1, **kwargs):
+        self.obs_dims = obs_dims
+
+        spaces = []
+        for name in self.obs_dims:
+            obs_dim = self.obs_dims[name]
+            obs_low = np.ones(obs_dim) * -1
+            obs_high = np.ones(obs_dim)
+            spaces.append((name, Box(obs_low, obs_high)))
+        self._observation_space = Dict(spaces)
+
+        self.action_dim = action_dim
+        action_low = np.ones(action_dim) * -1
+        action_high = np.ones(action_dim)
+        self._action_space = Box(action_low, action_high)
+
+        print("unused kwargs", kwargs)
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        obs = dict()
+        for name in self.obs_dims:
+            obs_dim = self.obs_dims[name]
+            obs[name] = np.zeros(obs_dim)
+        return obs, 0, 0, {}
 
     @property
     def action_space(self):
--- tests/regression/awac/gcrl/test_awac_gcrl_online.py
+++ tests/regression/awac/gcrl/test_awac_gcrl_online.py
@@ -7,7 +7,7 @@ import sys
 from experiments.references.awac.gcrl.pusher1 import main
 
 from rlkit.core import logger
-from rlkit.testing import csv_check
+from rlkit.testing import csv_util
 
 def test_awac_gcrl_online():
     cmd = "python experiments/references/awac/gcrl/pusher1.py --1 --local --gpu --run 0 --seed 0 --debug"
@@ -15,9 +15,10 @@ def test_awac_gcrl_online():
     main()
     reference_csv = "tests/regression/awac/gcrl/id0/progress.csv"
     output_csv = os.path.join(logger.get_snapshot_dir(), "progress.csv")
-    output = csv_check.get_exp(output_csv)
-    reference = csv_check.get_exp(reference_csv)
-    csv_check.check_equal(reference, output, ["epoch", "eval/Average Returns", "trainer/Advantage Score Max"], )
+    output = csv_util.get_exp(output_csv)
+    reference = csv_util.get_exp(reference_csv)
+    keys = ["epoch", "eval/Average Returns", "trainer/Advantage Score Max", "trainer/Q1 Predictions Mean", "trainer/replay_buffer_len"]
+    csv_util.check_equal(reference, output, keys)
 
 if __name__ == "__main__":
     test_awac_gcrl_online()
